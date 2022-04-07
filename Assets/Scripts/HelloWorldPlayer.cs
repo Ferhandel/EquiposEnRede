6bc0c6fc6d7024b3eb83d72b98c8fe17c9bd8b9d
Juan Fernández Temprano <fertemjua@IESPIRINGALLA.AD>
2022-04-07 20:58:07 +0200

    Limite de quipos empezado

--- Assets/Scripts/HelloWorldPlayer.cs
+++ Assets/Scripts/HelloWorldPlayer.cs
@@ -11,18 +11,16 @@ namespace HelloWorld
 
         //Lista de Colores
         public NetworkVariable<Color> colorPlayer= new NetworkVariable<Color>();
-        public static List<Color> avaliableColors = new List<Color>();
-
+        public NetworkVariable<int> numeroEquipo= new NetworkVariable<int>();
+        public static List<int> teams = new List<int>();
+        public static int maxPlayersOnTeam = 2;
+        //teams[numeroEquipo]
         Renderer ren;
 
         //START
         private void Start(){
             Position.OnValueChanged += OnPositionChange;
-            ren = GetComponent<Renderer>();
-
-            //SubmitNoTeamPositionServerRpc(true);
-            //SubmitRedTeamPositionServerRpc(true);
-            //SubmitBlueTeamPositionServerRpc(true);             
+            ren = GetComponent<Renderer>();           
         }   
         public void OnPositionChange(Vector3 previusValue, Vector3 newValue){
             transform.position = Position.Value;
@@ -42,21 +40,35 @@ namespace HelloWorld
         }
         [ServerRpc]
         void SubmitTeamPositionServerRpc(int team, ServerRpcParams rpcParams = default){
-            if (team == 0){
+            if (team == -1){
+                teams[0]++;
                 colorPlayer.Value = Color.white;
                 Position.Value = new Vector3(Random.Range(-1.5f,1.5f),1f,Random.Range(-3f,3f));
+                numeroEquipo.Value = 0;
                 Debug.Log("JUGADOR SIN EQUIPO");
-            }
-            if (team == 1){
+            }else if (team == 0){
+                teams[0]++;
+                colorPlayer.Value = Color.white;
+                Position.Value = new Vector3(Random.Range(-1.5f, 1.5f),1f,Random.Range(-3f,3f));
+                numeroEquipo.Value = 0;
+                Debug.Log("JUGADOR SIN EQUIPO");
+            }else if (team == 1){
+                teams[1]++;
                 colorPlayer.Value = Color.blue;
-                Position.Value = new Vector3(Random.Range(-3f, -1.5f),1f,Random.Range(-3f,3f));
-                Debug.Log("JUGADOR EQUIPO A AZUL");
-            }
-            if (team == 2){
+                Position.Value = new Vector3(Random.Range(-1.5f,-3f),1f,Random.Range(-3f,3f));
+                numeroEquipo.Value = 1;
+                Debug.Log("JUGADOR A EQUIPO AZUL");
+            } else if(team == 2){
+                if(teams[2] < maxPlayersOnTeam){
+                teams[2]++;
                 colorPlayer.Value = Color.red;
-                Position.Value = new Vector3(Random.Range(1.5f,3f),1f,Random.Range(-3f,3f));
+                Position.Value = new Vector3 (Random.Range(1.5f,3f),1f,Random.Range(-3f,3f));
+                numeroEquipo.Value = 2;
                 Debug.Log("JUGADOR A EQUIPO ROJO");
+              }
             }
+            
+            
         }
       
 
